d7c42fb35f84151a8b4db4dce4ac6dc8f32cfd88
Jack <jackson.virgo1@gmail.com>
2020-06-24 12:03:18 +0930

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=$(readlink -f $0 | xargs dirname)
-mv $BASEDIR/.bash_aliases $BASEDIR/.bash_aliases.bkp
-mv $BASEDIR/.bashrc $BASEDIR/.bashrc.bkp
+mv ~/.bash_aliases ~/.bash_aliases.bkp
+mv ~/.bashrc ~/.bashrc.bkp
 ln -s -r $BASEDIR/.bash_aliases ~/.bash_aliases
 ln -s -r $BASEDIR/.bashrc ~/.bashrc
